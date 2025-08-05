28dd2ca2e97fcffa390649372c047ae61cdbe72d
Mangal Singh <devv9585@gmail.com>
2025-08-06 00:13:16 +0530

    Add files via upload

---
+++ Schemas.sql.sql
@@ -0,0 +1,47 @@
+CREATE TABLE city(
+	city_id	INT PRIMARY KEY,
+	city_name	VARCHAR(30),
+	population	BIGINT,
+	estimated_rent	FLOAT,
+	city_rank INT
+);
+
+
+CREATE TABLE customers(
+	customer_id	INT PRIMARY KEY,
+	customer_name  VARCHAR(25),
+	city_id	INT ,
+	CONSTRAINT fk_city FOREIGN KEY (city_id) REFERENCES city(city_id)
+);
+
+
+CREATE TABLE products(
+	product_id	INT PRIMARY KEY,
+	product_name VARCHAR(50),
+	price FLOAT
+);
+
+
+
+CREATE TABLE sales(
+	sale_id	INT PRIMARY KEY,
+	sale_date	DATE,
+	product_id	INT,
+	customer_id	INT,
+	total FLOAT,
+	rating INT ,
+	CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES products(product_id),
+	CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+
+SELECT * FROM city;
+
+SELECT * FROM customers;
+
+SELECT * FROM products;
+
+SELECT * FROM sales;
+
+
+
